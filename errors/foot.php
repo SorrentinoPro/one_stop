7cb2fce09fd4dd23818c5fa5f46c21d2cc96a2e1
Francesco Sorrentino <francesco@sorrentino.ga>
2017-07-27 04:21:35 +0100

    This is the Stable Version.

---
+++ errors/foot.php
@@ -0,0 +1,9 @@
+		<div class="cont-foot">
+			<footer class="front-foot footer">
+				<p class="p-cpr" ><a style='color:#fff;' href="<?php echo $cpr_l; ?>"><?php echo $cpr_n; ?></a></p>
+			</footer>
+		</div>
+	    <script type="text/javascript" src="<?php echo $mysite; ?>/errors/js/jquery.js"></script>	
+		<script type="text/javascript" src="<?php echo $mysite; ?>/errors/js/errors.js"></script>		
+	</body>
+</html>
